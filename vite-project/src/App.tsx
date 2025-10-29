07096c3582b18611abf4214ef8bd434400f5a18d
wasijyayo <meganekaitai@gmail.com>
2025-10-29 09:30:59 +0900

    .mapで配置

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,14 +1,72 @@
+import { useState } from "react";
+
+type Todo = {
+  value: string;
+  readonly id: number;
+};
+
 export const App = () => {
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+  const handleEdit = (id: number, value: string) => {
+    setTodos((todos) => {
+      /**
+       * 引数として渡された todo の id が一致する
+       * 更新前の todos ステート内の todo の
+       * value プロパティを引数 value (= e.target.value) に書き換える
+       */
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          todo.value = value;
+        }
+        return todo;
+      });
+
+      // todos ステートを更新
+      return newTodos;
+    });
+  };
+
+  const handleSubmit = () => {
+    if (!text) return;
+
+    const newTodo: Todo = {
+      value: text,
+      id: new Date().getTime(),
+    };
+
+    setTodos((todos) => [newTodo, ...todos]);
+    setText("");
+  };
+
   return (
     <div>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" value="" onChange={(e) => e.preventDefault()} />
-        <input
-          type="submit"
-          value="追加"
-          onSubmit={(e) => e.preventDefault()}
-        />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <input type="text" value={text} onChange={(e) => handleChange(e)} />
+        <input type="submit" value="追加" onSubmit={handleSubmit} />
       </form>
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <input
+                type="text"
+                value={todo.value}
+                onChange={(e) => handleEdit(todo.id, e.target.value)}
+              />
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
